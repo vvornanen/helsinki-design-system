0c1970814f8d451e5a6af860c59c2dfeb51522a2
lehtoinen <lehtoinen@users.noreply.github.com>
2019-11-28 10:54:53 +0000

    Automated deployment: Thu Nov 28 10:54:53 UTC 2019 b93b8e256f330b5fbda6a0ea2c7bc2e3acb2d55c

---
+++ storybook/precache-manifest.937f7eb3d331d952547d31fea8d4a401.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ab06307f77ac89a03444c1baa241b5",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.54f84e9fcea8d5978345.bundle.js"
+  },
+  {
+    "revision": "98c4cc6e746092a2e4c5",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.54f84e9fcea8d5978345.bundle.js"
+  },
+  {
+    "revision": "a73385e3af548d10aae3406c40374c84",
+    "url": "static/media/placeholder_1400x1164.a73385e3.jpg"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "d828e0e6832531bd82af0376cb715adb",
+    "url": "static/media/placeholder_2_1920x1080.d828e0e6.jpg"
+  },
+  {
+    "url": "vendors~main.54f84e9fcea8d5978345.bundle.js"
+  }
+]);
