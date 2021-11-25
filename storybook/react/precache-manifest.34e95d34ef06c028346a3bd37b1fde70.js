99408ec77c6a45e87133a54bdb1339fc7f79d6c9
minevala <minevala@users.noreply.github.com>
2021-11-25 09:24:01 +0000

    Automated deployment: Thu Nov 25 09:24:00 UTC 2021 0258eb3cdf0bce97a339b4993713bf6d8f8eb1de

---
+++ storybook/react/precache-manifest.34e95d34ef06c028346a3bd37b1fde70.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca18653315c994b2456c",
+    "url": "0.34cc612b.iframe.bundle.js"
+  },
+  {
+    "revision": "7b958a0b2b4ed4619ca7",
+    "url": "4.3d771fa4.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.3d771fa4.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d041434c6f90af644f",
+    "url": "5.b5470275.iframe.bundle.js"
+  },
+  {
+    "revision": "637b09b2f5b15cc8f11c",
+    "url": "6.6b0509fc.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.6b0509fc.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb5851631e395bffb6a",
+    "url": "7.800894c0.iframe.bundle.js"
+  },
+  {
+    "revision": "b9c82be2ba91702f9aa7d7981c7ebe7b",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "1b32b3058fc03527d929",
+    "url": "main.d14d7a55.iframe.bundle.js"
+  },
+  {
+    "revision": "fedda70a396c6d8c536b",
+    "url": "runtime~main.5d6eccb6.iframe.bundle.js"
+  },
+  {
+    "revision": "1b32b3058fc03527d929",
+    "url": "static/css/main.d1f9297e.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "109e8c8e6884efb3ed11",
+    "url": "vendors~main.e57656a7.iframe.bundle.js"
+  },
+  {
+    "revision": "ad769daa05445f1ec9a3e6e1820e6338",
+    "url": "vendors~main.e57656a7.iframe.bundle.js.LICENSE.txt"
+  }
+]);
--- storybook/react/service-worker.js
+++ storybook/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d7ec29137643f5b219710081fec495cd.js"
+  "precache-manifest.34e95d34ef06c028346a3bd37b1fde70.js"
 );
 
 self.addEventListener('message', (event) => {
