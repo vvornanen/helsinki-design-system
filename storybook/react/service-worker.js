fe7f950a27c812ccbbcd61ad809f5e4f8ed6474d
VilleMiekkoja <VilleMiekkoja@users.noreply.github.com>
2021-11-29 08:04:32 +0000

    Automated deployment: Mon Nov 29 08:04:32 UTC 2021 6cca281ee2b15fa9674e9f496d9f72a453948a0c

--- storybook/react/service-worker.js
+++ storybook/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.50e441e7b5296d2ebbf94244ce870c55.js"
+  "precache-manifest.5102a02db62058a1b0b4002311b14ba3.js"
 );
 
 self.addEventListener('message', (event) => {
