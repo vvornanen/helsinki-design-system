6c43e1d1a646e14e1d1a46b7631e6f1caf545a8e
VilleMiekkoja <VilleMiekkoja@users.noreply.github.com>
2021-11-29 08:04:03 +0000

    Automated deployment: Mon Nov 29 08:04:03 UTC 2021 3f8e554349ec55957ae87aba36babc269460abee

---
+++ storybook/react/precache-manifest.50e441e7b5296d2ebbf94244ce870c55.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469506278798b391360f",
+    "url": "0.d8917d59.iframe.bundle.js"
+  },
+  {
+    "revision": "bfac5b0f6a76314a00c8",
+    "url": "4.e4a7a2e7.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.e4a7a2e7.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "17fe5cd7b70a51d9eb76",
+    "url": "5.4bc4eb4a.iframe.bundle.js"
+  },
+  {
+    "revision": "a330fa1786cd3ca95df7",
+    "url": "6.d95912f0.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.d95912f0.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1a9275fc81454ed17e",
+    "url": "7.f42d476d.iframe.bundle.js"
+  },
+  {
+    "revision": "42e54db46aac66663d1bfe90d3a05969",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "a548d7dd4606d8f6f1cc",
+    "url": "main.de037781.iframe.bundle.js"
+  },
+  {
+    "revision": "6648b0beabe633da7cb6",
+    "url": "runtime~main.90c63f14.iframe.bundle.js"
+  },
+  {
+    "revision": "a548d7dd4606d8f6f1cc",
+    "url": "static/css/main.6b4f014a.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "39a1043a368ab08023c0",
+    "url": "vendors~main.1ad5c366.iframe.bundle.js"
+  },
+  {
+    "revision": "ad769daa05445f1ec9a3e6e1820e6338",
+    "url": "vendors~main.1ad5c366.iframe.bundle.js.LICENSE.txt"
+  }
+]);
