5ee6b25b2e3fa4a3f9eea5cf678cad1444f82076
minevala <minevala@users.noreply.github.com>
2022-01-31 10:08:35 +0000

    Automated deployment: Mon Jan 31 10:08:35 UTC 2022 3f92a410d2b484d5db1d95cadf884fee8ab1a5e6

---
+++ storybook/react/precache-manifest.f2ba4aa3593ef233922900bc0f66431e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1113563f95f7637a7231",
+    "url": "0.5f15fc4c.iframe.bundle.js"
+  },
+  {
+    "revision": "cd29ff53673056576d81",
+    "url": "4.eb80befc.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.eb80befc.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "affda541025f602a3ab6",
+    "url": "5.36323783.iframe.bundle.js"
+  },
+  {
+    "revision": "99e3e729644859da1228",
+    "url": "6.fb72a3d1.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.fb72a3d1.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "9096f9676c03d045d907",
+    "url": "7.fb9f8aec.iframe.bundle.js"
+  },
+  {
+    "revision": "0b35ba2aa85e4ab700ad4600eeafd6dd",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "f56b9c8525115dbb89ee",
+    "url": "main.90de4a08.iframe.bundle.js"
+  },
+  {
+    "revision": "afc4dd77045f80ab57c0",
+    "url": "runtime~main.8222bc33.iframe.bundle.js"
+  },
+  {
+    "revision": "f56b9c8525115dbb89ee",
+    "url": "static/css/main.926ac00f.chunk.css"
+  },
+  {
+    "revision": "5df485b7948cd76506d7b090f4276a18",
+    "url": "static/media/placeholder-large.5df485b7.png"
+  },
+  {
+    "revision": "a11d6939cf6b39856116a08a3b768485",
+    "url": "static/media/placeholder-medium.a11d6939.png"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "5a9094def9f52f6ac8e0",
+    "url": "vendors~main.7299d7ae.iframe.bundle.js"
+  },
+  {
+    "revision": "ad769daa05445f1ec9a3e6e1820e6338",
+    "url": "vendors~main.7299d7ae.iframe.bundle.js.LICENSE.txt"
+  }
+]);
